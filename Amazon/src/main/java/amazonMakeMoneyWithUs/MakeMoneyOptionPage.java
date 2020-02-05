d2d42d977375459ee5c3fad9be62008c9972d6ea
saminaliza <58889891+saminaliza@users.noreply.github.com>
2020-02-04 23:46:45 -0500

    more tests added

---
+++ Amazon/src/main/java/amazonMakeMoneyWithUs/MakeMoneyOptionPage.java
@@ -0,0 +1,26 @@
+package amazonMakeMoneyWithUs;
+
+import base.CommonAPI;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class MakeMoneyOptionPage extends CommonAPI {
+
+//    @FindBy(linkText = "Sell on Amazon")
+//    WebElement sellOnAmazon;
+//    @FindBy(linkText = "Sell Under Amazon Accelerator")
+//    WebElement sellUnderAccelerator;
+//    @FindBy(linkText = "Sell on Amazon")
+//    WebElement sellOnAmazon;
+//    @FindBy(linkText = "Sell on Amazon")
+//    WebElement sellOnAmazon;
+//    @FindBy(linkText = "Sell on Amazon")
+//    WebElement sellOnAmazon;
+//    @FindBy(linkText = "Sell on Amazon")
+//    WebElement sellOnAmazon;
+//    @FindBy(linkText = "Sell on Amazon")
+//    WebElement sellOnAmazon;
+//    @FindBy(linkText = "Sell on Amazon")
+//    WebElement sellOnAmazon;
+
+}
--- Generic/src/main/java/databases/ConnectToSqlDB.java
+++ Generic/src/main/java/databases/ConnectToSqlDB.java
@@ -17,7 +17,7 @@ public class ConnectToSqlDB {
 
     public static Properties loadProperties() throws IOException{
         Properties prop = new Properties();
-        InputStream ism = new FileInputStream("..\\Generic\\secret.properties");
+        InputStream ism = new FileInputStream("C:\\Users\\samin\\Desktop\\projectRN\\SeleniumProject_Squad\\Generic\\secret.properties");
         prop.load(ism);
         ism.close();
         return prop;
